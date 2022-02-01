ea0daf4aba8ac539d18556a6b12af3e2eef8ebbc
William Warner Burke <williamburke121@gmail.com>
2022-02-01 10:35:08 -0500

    commit tree 5a992e6df88ae042a3dbc9eec7ebe79700eeec22

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = [];
+drivers [0] = "Antonia"
+drivers [1] = "Nuru"
+drivers [2] = "Amari"
+drivers [3] = "Mo"
+function returnFirstTwoDrivers(){
+    return (["Antonia","Nuru"])
+}
+
+function returnLastTwoDrivers(){
+    return (["Amari", "Mo"])
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+
+function createFareMultiplier(x){
+    return function(fare){
+        return fare * x
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, driverFunction){
+    return driverFunction(arrayOfDrivers)
+}
